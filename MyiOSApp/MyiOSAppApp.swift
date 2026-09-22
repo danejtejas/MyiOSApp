d6d2ecbb982e26752b63cf59dd81c15838a21296
tejas <danej.tejash0@gmail.com>
2026-09-22 15:43:28 +0530

    Inital commit

---
+++ MyiOSApp/MyiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyiOSAppApp.swift
+//  MyiOSApp
+//
+//  Created by Tejas on 21/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MyiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
